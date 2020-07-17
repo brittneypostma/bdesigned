e5e13157b9b9256a7ca5e1504e77d6240be76f3e
Brittney <brittney.postma@hotmail.com>
2020-07-17 10:22:22 -0400

    ran format on all files not ignored

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,17 @@
 module.exports = {
-  "extends": ["eslint:recommended", "prettier"],
+  extends: ['eslint:recommended', 'prettier'],
   parserOptions: {
     ecmaVersion: 2020,
-    sourceType: 'module'
+    sourceType: 'module',
   },
   env: {
     es6: true,
     browser: true,
     node: true,
   },
-  plugins: [
-    'svelte3',
-    'prettier'
-  ],
-  "rules": {
-    "no-unused-vars": ["error", { "args": "none" }],
-    "no-console": "on"
+  plugins: ['svelte3', 'prettier'],
+  rules: {
+    'no-unused-vars': ['error', { args: 'none' }],
+    'no-console': 'on',
   },
 }
--- .prettierrc.js
+++ .prettierrc.js
@@ -5,5 +5,5 @@ module.exports = {
   svelteBracketNewLine: true,
   tabWidth: 2,
   singleQuote: true,
-  plugins: ['prettier-plugin-svelte']
+  plugins: ['prettier-plugin-svelte'],
 }
