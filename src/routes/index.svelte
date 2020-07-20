5b0adfdb903390460edf5910888a78295df66903
Brittney <brittney.postma@hotmail.com>
2020-07-20 09:21:42 -0400

    background in

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,51 +2,14 @@
   <title>Sapper project template</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<div class="bg w-screen">
+  <h1>Great success!</h1>
 
-<figure class="grid justify-center">
-  <img alt="Success Kid" src="successkid.jpg" />
-  <figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p>
-  <strong>
-    Try editing this file (src/routes/index.svelte) to test live reloading.
-  </strong>
-</p>
+</div>
 
 <style>
-  h1,
-  figure,
-  p {
-    text-align: center;
-    margin: 0 auto;
-  }
-
-  h1 {
-    font-size: 2.8em;
-    text-transform: uppercase;
-    font-weight: 700;
-    margin: 0 0 0.5em 0;
-  }
-
-  figure {
-    margin: 0 0 1em 0;
-  }
-
-  img {
-    width: 100%;
-    max-width: 400px;
-    margin: 0 0 1em 0;
-  }
-
-  p {
-    margin: 1em auto;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      font-size: 4em;
-    }
+  .bg {
+    background: url('/wave.png') no-repeat fixed bottom right;
+    height: calc(100vh - 57px);
   }
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,20 @@
 module.exports = {
   purge: ["./src/**/*.svelte"],
   theme: {
+    colors: {
+      white: "#ffffff",
+      sand: "EBE4DC",
+      "light-blue": "#E8F4F4",
+      mint: "#AEEADE",
+      caribbean: "#00E0FF",
+      blue: "#5FC7E4",
+      "light-teal": "#68D2C8",
+      teal: "#00789E",
+      yellow: "#E5A91E",
+      navy: "#1B2A49",
+      gray: "#333333",
+      black: "#000000"
+    },
     extend: {},
   },
   variants: {},
