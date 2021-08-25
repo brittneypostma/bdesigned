72413c74b31d46db59e064105328eec9a2ece27a
brittneypostma <brittney.postma@hotmail.com>
2021-08-24 20:39:43 -0400

    fixed img issue

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,105 +1,113 @@
 <script>
-  import { fly } from 'svelte/transition'
+	import { fly } from 'svelte/transition'
 
-  const loaded = new Map()
+	const loaded = new Map()
 
-  function lazy(node, data) {
-    if (loaded.has(data.src)) {
-      node.setAttribute('src', data.src)
-    } else {
-      // simulate slow loading network
-      setTimeout(() => {
-        const img = new Image()
-        img.src = data.src
-        img.onload = () => {
-          loaded.set(data.src, img)
-          node.setAttribute('src', data.src)
-        }
-      }, 2000)
-    }
+	function lazy(node, data) {
+		if (loaded.has(data.src)) {
+			node.setAttribute('src', data.src)
+		} else {
+			// simulate slow loading network
+			setTimeout(() => {
+				const img = new Image()
+				img.src = data.src
+				img.onload = () => {
+					loaded.set(data.src, img)
+					node.setAttribute('src', data.src)
+				}
+			}, 2000)
+		}
 
-    return {
-      destroy() {}, // noop
-    }
-  }
+		return {
+			destroy() {} // noop
+		}
+	}
+
 </script>
 
 <svelte:head>
-  <title>About | bDesigned</title>
+	<title>About | bDesigned</title>
 </svelte:head>
 
 <img
-  use:lazy="{{ src: 'wave.png' }}"
-  src="wave.png"
-  alt="wave"
-  class="fixed bottom-0 right-0 z-0 hidden lg:block"
-/>
+	use:lazy={{ src: 'wave.png' }}
+	src="wave.png"
+	alt="wave"
+	class="fixed bottom-0 right-0 z-0 hidden lg:block" />
 <div
-  in:fly="{{ y: 500, duration: 700 }}"
-  class="h-full p-12 lg:max-w-md xl:max-w-6xl 2xl:mx-auto text-wrapper"
->
-  <h1>About Me</h1>
-  <img
-    use:lazy="{{ src: 'me.png' }}"
-    src="bg.png"
-    alt="Brittney"
-    class="mt-10 rounded-full shadow-2xl lg:float-left lg:m-12"
-  />
-  <p>
-    I'm Brittney Postma, the developer and designer behind bDesigned. I have a background in graphic design and more recently found my passion for coding. I taught myself through the <a href="https://academy.zerotomastery.io/?affcode=441520_gjue7n-1">ZTM Academy</a> while being a stay at home mom to my 3 wonderful kids. I am a star mentor in the ZTM Discord community and recently joined the other great instructors at the Academy.  I did a live workshop on Responsive Web Design that is available to all Academy students now! 
-  </p>
-  <p>
-    I love developing beautiful, fluid, and responsive websites using new technologies. I thrive on accessible, semantic HTML that also has personality. I want everyone to be included on my websites, but also have some fun!
-    I use Figma to create design systems, mockups, and clickable prototypes. User experience is extremely important to me and I strive to have a low user misery rating. I also use Photoshop like tools to create logos and high quality SVG icons and designs.
-  </p>
-  <p>
-    I am passionate about UI/UX (User Interface and User Experience) development and SEO. I have extensive experience with frontend frameworks  React/Gatsby and Svelte/Sapper on the <a href="https://jamstack.org/">Jamstack</a>. I have experience working with a wide range of technologies including Vue, React
-    Native, Python, GraphQL, and SQL and NoSQL databases. 
-  </p>
-  <p>
-    I created my own VSCode theme, <a href="https://marketplace.visualstudio.com/items?itemName=bDesigned.deep-water">Deep Water</a>, based on the colors of the ocean, and wrote about the process over at <a href="https://dev.to/bdesigned/build-a-vscode-theme-3f4i">dev.to</a>.
-    I stream and record videos on
-    <a href="https://www.youtube.com/channel/UCyvOaBoW3Jti69U4Gw1ci9Q">
-      Youtube
-    </a> and really enjoy teaching others all the awesome things I have learned. I
-    started a digital garden,
-    <a href="https://www.theconsolelogs.com/">The Console Logs</a>
-    , as a place to keep all of my development notes. If you would like to get in touch, you can reach me at the socials below or
-    <a href="contact">send me a message</a>
-    !
-  </p>
+	in:fly={{ y: 500, duration: 700 }}
+	class="h-full p-12 lg:max-w-md xl:max-w-6xl 2xl:mx-auto text-wrapper">
+	<h1>About Me</h1>
+	<img src="me.png" alt="Brittney" class="mt-10 rounded-full shadow-2xl lg:float-left lg:m-12" />
+	<p>
+		I'm Brittney Postma, the developer and designer behind bDesigned. I have a background in graphic
+		design and more recently found my passion for coding. I taught myself through the
+		<a href="https://academy.zerotomastery.io/?affcode=441520_gjue7n-1">ZTM Academy</a>
+		while being a stay at home mom to my 3 wonderful kids. I am a star mentor in the ZTM Discord
+		community and recently joined the other great instructors at the Academy. I did a live workshop
+		on Responsive Web Design that is available to all Academy students now!
+	</p>
+	<p>
+		I love developing beautiful, fluid, and responsive websites using new technologies. I thrive on
+		accessible, semantic HTML that also has personality. I want everyone to be included on my
+		websites, but also have some fun! I use Figma to create design systems, mockups, and clickable
+		prototypes. User experience is extremely important to me and I strive to have a low user misery
+		rating. I also use Photoshop like tools to create logos and high quality SVG icons and designs.
+	</p>
+	<p>
+		I am passionate about UI/UX (User Interface and User Experience) development and SEO. I have
+		extensive experience with frontend frameworks React/Gatsby and Svelte/Sapper on the
+		<a href="https://jamstack.org/">Jamstack</a>. I have experience working with a wide range of
+		technologies including Vue, React Native, Python, GraphQL, and SQL and NoSQL databases.
+	</p>
+	<p>
+		I created my own VSCode theme,
+		<a href="https://marketplace.visualstudio.com/items?itemName=bDesigned.deep-water">Deep Water</a>,
+		based on the colors of the ocean, and wrote about the process over at
+		<a href="https://dev.to/bdesigned/build-a-vscode-theme-3f4i">dev.to</a>. I stream and record
+		videos on
+		<a href="https://www.youtube.com/channel/UCyvOaBoW3Jti69U4Gw1ci9Q"> Youtube </a>
+		and really enjoy teaching others all the awesome things I have learned. I started a digital
+		garden,
+		<a href="https://www.theconsolelogs.com/">The Console Logs</a>
+		, as a place to keep all of my development notes. If you would like to get in touch, you can
+		reach me at the socials below or
+		<a href="contact">send me a message</a>
+		!
+	</p>
 </div>
 
 <style>
-  .text-wrapper {
-    color: var(--about-text);
-  }
+	.text-wrapper {
+		color: var(--about-text);
+	}
 
-  img {
-    z-index: -10;
-  }
+	img {
+		z-index: -10;
+	}
 
-  p:not(:nth-child(2)) {
-    margin-top: 40px;
-  }
-  a {
-    color: var(--accent-color);
-    text-decoration: none;
-    position: relative;
-  }
+	p:not(:nth-child(2)) {
+		margin-top: 40px;
+	}
+	a {
+		color: var(--accent-color);
+		text-decoration: none;
+		position: relative;
+	}
 
-  a::before {
-    content: '';
-    position: absolute;
-    transition: all 0.3s ease;
-    bottom: 0;
-    width: 100%;
-    height: 2px;
-    background: var(--caribbean);
-    transform: scaleX(0);
-  }
+	a::before {
+		content: '';
+		position: absolute;
+		transition: all 0.3s ease;
+		bottom: 0;
+		width: 100%;
+		height: 2px;
+		background: var(--caribbean);
+		transform: scaleX(0);
+	}
 
-  a:hover::before {
-    transform: scaleX(1);
-  }
+	a:hover::before {
+		transform: scaleX(1);
+	}
+
 </style>
--- src/routes/portfolio.svelte
+++ src/routes/portfolio.svelte
@@ -1,120 +1,114 @@
 <script>
-  import { items } from '../data/portfolio.js'
-  import { fade } from 'svelte/transition'
+	import { items } from '../data/portfolio.js'
+	import { fade } from 'svelte/transition'
 
-  const loaded = new Map()
+	const loaded = new Map()
 
-  let visible = false
+	let visible = false
 
-  function lazy(node, data) {
-    if (loaded.has(data.src)) {
-      node.setAttribute('src', data.src)
-    } else {
-      // simulate slow loading network
-      setTimeout(() => {
-        const img = new Image()
-        img.src = data.src
-        img.onload = () => {
-          loaded.set(data.src, img)
-          node.setAttribute('src', data.src)
-        }
-      }, 2000)
-    }
+	function lazy(node, data) {
+		if (loaded.has(data.src)) {
+			node.setAttribute('src', data.src)
+		} else {
+			// simulate slow loading network
+			setTimeout(() => {
+				const img = new Image()
+				img.src = data.src
+				img.onload = () => {
+					loaded.set(data.src, img)
+					node.setAttribute('src', data.src)
+				}
+			}, 2000)
+		}
 
-    return {
-      destroy() {}, // noop
-    }
-  }
+		return {
+			destroy() {} // noop
+		}
+	}
+
 </script>
 
 <svelte:head>
-  <title>Portfolio | bDesigned</title>
+	<title>Portfolio | bDesigned</title>
 </svelte:head>
 
-<div in:fade="{{ duration: 1000 }}" class="p-0 cont sm:p-12 2xl:mx-auto max-w-8xl">
-  <h1 class="ml-6">Portfolio</h1>
-  <div class="mt-6 blocks md:mt-12">
-    {#each items as item}
-      <div
-        class="grid my-10 rounded-lg content md:relative md:m-0 md:p-0 md:overflow-hidden"
-      >
-        <a href="{item.link}">
-          <div class="bg-opacity-75 content-overlay bg-navy"></div>
-          <img
-            use:lazy="{{ src: item.image }}"
-            src="{item.img}"
-            alt="{item.alt}"
-          />
-          <div
-            class="leading-relaxed content-details fadeIn-bottom text-navy md:text-sand"
-          >
-            <h4 class="text-center content-title">{item.alt}</h4>
-            <p class="text-center">{item.desc}</p>
-          </div>
-        </a>
-      </div>
-    {/each}
-  </div>
+<div in:fade={{ duration: 1000 }} class="p-0 cont sm:p-12 2xl:mx-auto max-w-8xl">
+	<h1 class="ml-6 mt-6 lg:mt-0">Portfolio</h1>
+	<div class="px-4 md:px-0 mt-6 blocks md:mt-12">
+		{#each items as item}
+			<div class="grid my-10 rounded-lg content md:relative md:m-0 md:p-0 md:overflow-hidden">
+				<a href={item.link}>
+					<div class="bg-opacity-75 content-overlay bg-navy" />
+					<img use:lazy={{ src: item.image }} src={item.img} alt={item.alt} />
+					<div class="leading-relaxed content-details fadeIn-bottom text-navy md:text-sand">
+						<h4 class="text-center content-title">{item.alt}</h4>
+						<p class="text-center">{item.desc}</p>
+					</div>
+				</a>
+			</div>
+		{/each}
+	</div>
 </div>
 
 <style>
-  .content-title {
-    margin: 0 auto;
-  }
+	.content-title {
+		margin: 0 auto;
+	}
 
-  @media only screen and (min-width: 768px) {
-    .blocks {
-      padding: 0;
-      display: flex;
-      flex-wrap: wrap;
-      gap: 1rem;
-    }
-    .content {
-      flex: 1 1 500px;
-    }
-    .content-details {
-      position: absolute;
-      text-align: center;
-      padding: 0;
-      width: 75%;
-      top: 50%;
-      left: 50%;
-      opacity: 0;
-      -webkit-transform: translate(-50%, -50%);
-      -moz-transform: translate(-50%, -50%);
-      transform: translate(-50%, -50%);
-      -webkit-transition: all 0.3s ease-in-out 0s;
-      -moz-transition: all 0.3s ease-in-out 0s;
-      transition: all 0.3s ease-in-out 0s;
-    }
-    .content-overlay {
-      /* background-image: linear-gradient(to bottom, var(--blue), var(--navy) 90%); */
-      position: absolute;
-      height: 100%;
-      width: 100%;
-      left: 0;
-      top: 0;
-      bottom: 0;
-      right: 0;
-      opacity: 0;
-      z-index: 2;
-      -webkit-transition: all 0.4s ease-in-out 0s;
-      -moz-transition: all 0.4s ease-in-out 0s;
-      transition: all 0.4s ease-in-out 0s;
-    }
-    .content:hover .content-details {
-      top: 50%;
-      left: 50%;
-      opacity: 1;
-      z-index: 3;
-    }
+	@media only screen and (min-width: 768px) {
+		.blocks {
+			padding: 0;
+			display: flex;
+			flex-wrap: wrap;
+			gap: 1rem;
+		}
+		.content {
+			flex: 1 1 500px;
+		}
+		.content-details {
+			position: absolute;
+			text-align: center;
+			padding: 0;
+			width: 75%;
+			top: 50%;
+			left: 50%;
+			opacity: 0;
+			-webkit-transform: translate(-50%, -50%);
+			-moz-transform: translate(-50%, -50%);
+			transform: translate(-50%, -50%);
+			-webkit-transition: all 0.3s ease-in-out 0s;
+			-moz-transition: all 0.3s ease-in-out 0s;
+			transition: all 0.3s ease-in-out 0s;
+		}
+		.content-overlay {
+			/* background-image: linear-gradient(to bottom, var(--blue), var(--navy) 90%); */
+			position: absolute;
+			height: 100%;
+			width: 100%;
+			left: 0;
+			top: 0;
+			bottom: 0;
+			right: 0;
+			opacity: 0;
+			z-index: 2;
+			-webkit-transition: all 0.4s ease-in-out 0s;
+			-moz-transition: all 0.4s ease-in-out 0s;
+			transition: all 0.4s ease-in-out 0s;
+		}
+		.content:hover .content-details {
+			top: 50%;
+			left: 50%;
+			opacity: 1;
+			z-index: 3;
+		}
 
-    .content:hover .content-overlay {
-      opacity: 1;
-    }
+		.content:hover .content-overlay {
+			opacity: 1;
+		}
 
-    .fadeIn-bottom {
-      top: 80%;
-    }
-  }
+		.fadeIn-bottom {
+			top: 80%;
+		}
+	}
+
 </style>
